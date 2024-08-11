43950974b442f015ab033cb002fdef39a967aee0
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-08-11 11:46:45 -0300

    fix: order item total

--- src/entity/order.test.ts
+++ src/entity/order.test.ts
@@ -22,14 +22,14 @@ describe("Order unit tests", () => {
     });
 
     it("should calculate total" , () => {
-        const item1 = new OrderItem("i1", "Item 1", 100);
+        const item1 = new OrderItem("i1", "Item 1", 100, 1);
         const order = new Order("o1", "c1", [item1]);
 
         let total = order.total();
 
         expect(total).toBe(100);
 
-        const item2 = new OrderItem("i1", "Item 1", 200);
+        const item2 = new OrderItem("i1", "Item 1", 200, 1);
         const order2 = new Order("o2", "c1", [item1, item2]);
 
         total = order2.total();
--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,12 +1,26 @@
 export class OrderItem {
 
-    _id: string;
-    _name: string;
-    _price: number;
+    private _id: string;
+    private _name: string;
+    private _price: number;
+    private _quantity: number;
 
-    constructor(id: string, name: string, price: number) {
+    constructor(id: string, name: string, price: number, quantity: number) {
         this._id =id;
         this._name = name;
         this._price = price;
+        this._quantity = quantity;
+    }
+
+    get price(): number {
+        return this._price;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    orderItemTotal(): number {
+        return this._price * this._quantity;
     }
 }
