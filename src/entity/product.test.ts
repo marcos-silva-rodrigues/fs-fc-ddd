f6a19c7932299ccb1dae2176854bf49c29f8062c
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-08-11 11:43:30 -0300

    test: create and test product

---
+++ src/entity/product.test.ts
@@ -0,0 +1,37 @@
+import { Product } from "./product";
+
+describe("Product unit tests", () => {
+    it("should throw error when id is empty" , () => {
+        expect(() => {
+            const product = new Product("", "Product 1", 100);
+        }).toThrow("Id is required");
+    });
+
+    it("should throw error when name is empty" , () => {
+        expect(() => {
+            const product = new Product("p1", "", 100);
+        }).toThrow("Name is required");
+    });
+
+    
+    it("should throw error when price is less than 0" , () => {
+        expect(() => {
+            const product = new Product("p1", "Product 1", 0);
+        }).toThrow("Price must be greater than 0");
+    });
+
+    it("should change name" , () => {
+        const product = new Product("p1", "Product 1", 20);
+
+        product.changeName("Product 2");
+        expect(product.name).toBe("Product 2");
+    });
+
+    it("should change price" , () => {
+        const product = new Product("p1", "Product 1", 100);
+
+        product.changePrice(150);
+        expect(product.price).toBe(150);
+    });
+
+});
