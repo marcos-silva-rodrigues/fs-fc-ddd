590eaf3d34aa6beebf3b8681a322c13ece2469ff
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-08-16 18:56:54 -0300

    refactor: add modules

---
+++ src/domain/customer/event/handler/envia-console-log.handler.ts
@@ -0,0 +1,10 @@
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
+import CustomerUpdatedEvent from "../customer-updated.event";
+
+export default class EnviaConsoleLogHandler 
+    implements EventHandlerInterface<CustomerUpdatedEvent> {
+    
+    handle(event: CustomerUpdatedEvent): void {
+        console.log(`Endereço do cliente: ${event.eventData?.id}, ${event.eventData?.name} alterado para: ${event.eventData?.endereco}`);
+    }
+}
