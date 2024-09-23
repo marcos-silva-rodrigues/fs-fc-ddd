f6e3adf8dae1584511075c58cec1f025b76c6fcf
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-09-23 09:38:53 -0300

    feat: integration test for creat a product

---
+++ src/usecase/product/create/create.product.usecase.integration.test.ts
@@ -0,0 +1,39 @@
+import { Sequelize } from "sequelize-typescript";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import CreateProductUseCase from "./create.customer.usecase";
+
+describe("Test find customer use case", () => {
+    let sequelize: Sequelize;
+
+    beforeEach(async () => {
+        sequelize = new Sequelize({
+            dialect: "sqlite",
+            storage: ":memory:",
+            logging: false,
+            sync: { force: true },
+        });
+
+        sequelize.addModels([ProductModel]);
+        await sequelize.sync();
+    });
+
+    afterEach(async () => {
+        await sequelize.close();
+    });
+
+    it("should create a customer", async () => {
+        const productRepository = new ProductRepository();
+        const usecase = new CreateProductUseCase(productRepository);
+
+        const input = {
+            name: "Product 1",
+            price: 10
+        }
+        const output = await usecase.execute(input);
+
+        expect(output.id).toEqual(expect.any(String));
+        expect(output.name).toBe(input.name);
+        expect(output.price).toBe(input.price);
+    })
+})
