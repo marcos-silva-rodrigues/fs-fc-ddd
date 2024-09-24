fd6e2c34d5746d24ec2ed043850c09b73e3fcada
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-09-24 11:04:02 -0300

    feat: create server and customer routes

---
+++ src/infrastructure/api/__tests__/customer.e2e.test.ts
@@ -0,0 +1,106 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for customer", () => {
+    beforeEach(async () => {
+        await sequelize.sync({force: true});
+    })
+
+    afterAll(async () => {
+        await sequelize.close();
+    })
+
+    it("should create a customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "Jhon",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "123454"
+                }
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body.name).toBe("Jhon");
+        expect(response.body.address.street).toBe("Street");
+        expect(response.body.address.city).toBe("City");
+        expect(response.body.address.number).toBe(123);
+        expect(response.body.address.zip).toBe("123454");
+    })
+
+    it("should not create a customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "123454"
+                }
+            });
+
+        expect(response.status).toBe(500);
+        expect(response.text).toBe("Name is required");
+    })
+
+    it("should list all customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "Jhon",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "123454"
+                }
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body.name).toBe("Jhon");
+        expect(response.body.address.street).toBe("Street");
+        expect(response.body.address.city).toBe("City");
+        expect(response.body.address.number).toBe(123);
+        expect(response.body.address.zip).toBe("123454");
+
+        const response2 = await request(app)
+            .post("/customer")
+            .send({
+                name: "Jhon 2",
+                address: {
+                    street: "Street 2",
+                    city: "City 2",
+                    number: 321,
+                    zip: "43215"
+                }
+            });
+
+        expect(response2.status).toBe(201);
+        expect(response2.body.name).toBe("Jhon 2");
+        expect(response2.body.address.street).toBe("Street 2");
+        expect(response2.body.address.city).toBe("City 2");
+        expect(response2.body.address.number).toBe(321);
+        expect(response2.body.address.zip).toBe("43215");
+
+        const listResponse = await request(app)
+            .get("/customer")
+            .send();
+        console.log(listResponse.body)
+
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.customers.length).toBe(2);
+
+        const customer1 = listResponse.body.customers[0];
+        expect(customer1.name).toBe("Jhon");
+        expect(customer1.address.street).toBe("Street");
+
+        const customer2 = listResponse.body.customers[1];
+        expect(customer2.name).toBe("Jhon 2");
+        expect(customer2.address.street).toBe("Street 2");
+    })
+});
---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from "express";
+import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
+import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import ProductRepository from "../../product/repository/sequelize/product.repository";
+
+export const customerRoute = express.Router();
+
+customerRoute.post("/", async (req: Request, res: Response) => {
+    const usecase = new CreateCustomerUseCase(new CustomerRepository());
+
+    try {
+        const customerDto = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip
+            }
+        }
+
+        const output = await usecase.execute(customerDto);
+        res.status(201).send(output);
+    } catch (err) {
+        const { message } = err as Error;
+        res.status(500).send(message);
+    }
+})
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+    const usecase = new ListCustomerUseCase(new CustomerRepository());
+
+
+    try {
+        const output = await usecase.execute({});
+        return res.send(output);
+    } catch (err) {
+        const { message } = err as Error;
+        res.status(500).send(message);
+    }
+})
